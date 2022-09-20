0cfc6975294eb2b8006ee9aa1b8a79192e04ae9d
bug132008 <nhaneye@gmail.com>
2022-09-20 17:02:33 -0300

    final 2

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-
+poss = ["easy", "medium", "hard", "impossivel"]
 app = Flask(__name__)
 @app.route('/')
 def home():
@@ -8,6 +8,10 @@ def home():
 @app.route('/<diff>/')
 @app.route('/<diff>')
 def easy(diff):
-	return render_template(f'{diff}/index.html')
+	for possi in poss:
+		if possi == diff:
+			return render_template(f'{diff}/index.html')
+		else:
+			pass
 if __name__ == "__main__":
 	app.run(debug=True, port=8080)
