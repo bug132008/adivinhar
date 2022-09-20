dce25da5ae352dfc90c13f198f103680e15b424f
bug132008 <nhaneye@gmail.com>
2022-09-20 16:56:44 -0300

    final 1

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ from flask import Flask, render_template
 app = Flask(__name__)
 @app.route('/')
 def home():
-	return render_template('easy/index.html')
+	return render_template("home/index.html")
 
+@app.route('/<diff>/')
+@app.route('/<diff>')
+def easy(diff):
+	return render_template(f'{diff}/index.html')
 if __name__ == "__main__":
 	app.run(debug=True, port=8080)
---
+++ static/js/index.js
@@ -0,0 +1,14 @@
+function ir(param){
+  if(param == "easy"){
+    window.location.href = 'easy'
+  }
+  if(param == "medio"){
+    window.location.href = 'medium'
+  }
+  if(param == "hard"){
+    window.location.href = "hard"
+  }
+  if(param == "impossivel"){
+    window.location.href = "impossivel"
+  }
+}
